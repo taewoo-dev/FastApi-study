f43deb269b2408c1c279ef7ebb7805c61a6155fc
taewoo-dev <twin9774@gmail.com>
2024-07-13 18:39:17 +0900

    user,events routes setting

--- planner/models/users.py
+++ planner/models/users.py
@@ -12,16 +12,15 @@ class User(BaseModel):
     password: 사용자 패스워드
     events: 해당 사용자가 생성한 이벤트. 처음에는 비어 있다
     """
-    email: EmailStr
-    password: str
+    email: EmailStr = Field(...)
+    password: str = Field(...)
     events: Optional[List[Event]] = Field(default=None)
 
-    class Config:
+    class Config: # 문서 출력 순서가 알파벳 순으로 정렬된다.
         json_schema_extra = {
             "example": {
                 "email": "fastapi@packt.com",
                 "password": "strong!!!",
-                "events": []
             }
         }
 
@@ -34,7 +33,6 @@ class UserSignIn(BaseModel):
         json_schema_extra = {
             "example": {
                 "email": "fastapi@packt.com",
-                "password": "strong!!!",
-                "events": [],
+                "password": "strong!!!"
             }
         }
